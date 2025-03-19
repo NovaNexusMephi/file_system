1f47ab530e112c3cb35079b0384579c60e5927b9
10Narratives <vanyatertyshnikov@yandex.ru>
2025-03-19 11:22:23 +0300

    Refactor code: replace config lib

---
+++ config/src/config.cpp
@@ -0,0 +1,15 @@
+#include "config/config.h"
+
+#include <yaml-cpp/node/parse.h>
+
+#include <filesystem>
+
+using config::Config;
+
+Config::Config(const std::filesystem::path& cfg_path) {
+  if (std::filesystem::exists(cfg_path)) {
+    auto cfg = YAML::LoadFile(cfg_path);
+    cfg["fat"] >> fat_cfg_;
+    cfg["info"] >> info_cfg_;
+  }
+}
