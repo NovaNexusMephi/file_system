b7a51596c158ea43748702fa9294d32312be08e2
10Narratives <vanyatertyshnikov@yandex.ru>
2025-04-20 12:42:23 +0300

    Refactor code: update Monitor methods

--- src/monitor/include/monitor/monitor.h
+++ src/monitor/include/monitor/monitor.h
@@ -2,7 +2,6 @@
 
 #include <nlohmann/json.hpp>
 #include <optional>
-#include <ostream>
 #include "monitor/logger_manager.h"
 
 #include <config/monitor_config.h>
@@ -11,15 +10,11 @@ namespace monitor {
 class Monitor final {
    public:
     Monitor();
-    ~Monitor();
 
     [[nodiscard]] std::optional<nlohmann::json> poll_command();
     void display(const std::string& message);
 
    private:
-    std::string get_current_time();
-    void write(std::ostream& stream, const std::string& message);
-
     std::string last_command_{};
 
     LoggerManager l_manager_{};
--- src/monitor/source/monitor.cpp
+++ src/monitor/source/monitor.cpp
@@ -1,12 +1,8 @@
 #include "monitor/monitor.h"
-#include <chrono>
 #include <ctime>
-#include <iomanip>
 #include <iostream>
 #include <nlohmann/json_fwd.hpp>
 #include <optional>
-#include <ostream>
-#include <sstream>
 #include <string>
 #include "monitor/command_parsing.h"
 
@@ -17,8 +13,6 @@ Monitor::Monitor() : l_manager_("logs") {
     l_manager_.create_logger("results", "results.log");
 }
 
-Monitor::~Monitor() {}
-
 std::optional<nlohmann::json> Monitor::poll_command() {
     std::string command_line;
     if (!(std::getline(std::cin, command_line, ';'))) {
@@ -27,21 +21,6 @@ std::optional<nlohmann::json> Monitor::poll_command() {
     last_command_ = command_line + ";";
 
     return parse_command(last_command_);
-}
-
-void Monitor::write(std::ostream& stream, const std::string& msg) {
-    stream << msg << std::endl;
-    stream.flush();
-}
-
-std::string Monitor::get_current_time() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t current_time = std::chrono::system_clock::to_time_t(now);
-
-    std::ostringstream oss;
-    oss << std::put_time(std::localtime(&current_time), "%Y-%m-%d_%H:%M:%S");
-
-    return oss.str();
 }
 
 void Monitor::display(const std::string& message) {
