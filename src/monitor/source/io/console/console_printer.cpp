4c532f02f9adbef9c534bd7e4e4112637dfa32e2
10Narratives <vanyatertyshnikov@yandex.ru>
2025-04-27 09:10:33 +0300

    Start to make monitor

--- src/monitor/source/io/console/console_printer.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "monitor/io/console/console_printer.hpp"
-using monitor::io::console::ConsolePrinter;
-
-void ConsolePrinter::print(const std::string& msg) {
-    std::cout << msg;
-}
--- src/monitor/source/io/file/file_printer.cpp
+++
@@ -1,4 +0,0 @@
-#include "monitor/io/file/file_printer.hpp"
-using monitor::io::file::FilePrinter;
-
-FilePrinter::FilePrinter(const std::filesystem::path& f_path) {}
