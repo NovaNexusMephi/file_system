62ffeff3f7867962546d52f700a874282e3b660c
pErfEcto2 <misha.yaro2004@gmail.com>
2025-04-29 20:12:08 +0300

    configure.sh script added

---
+++ configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ "$EUID" -ne 0 ] then 
+	echo "you should run this script as root"
+  exit
+fi
+
+set -e
+
+apt update -y 
+apt install -y make=4.3-4.1build1 \
+							 cmake=3.22.1-1ubuntu1.22.04.2 \
+							 gcc=4:11.2.0-1ubuntu1 \
+							 pkg-config=0.29.2-1ubuntu3 \
+							 libconfig++-dev=1.5-0.4build1 \
+							 libgtest-dev=1.11.0-3 \
+							 nlohmann-json3-dev=3.10.5-2 \
+							 clang=1:14.0-55~exp2
+
+
