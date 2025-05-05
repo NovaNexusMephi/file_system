35439d36f6cd669f0a5af01798f89896a7dfe495
MaximKyznetsov <maxim.kuznetsov@maddevs.io>
2025-05-05 23:07:58 +0300

    Revert "Fix delete function"
    This reverts commit 7d51a0e55c8814f528f0b7b84a0561052330fbfc.

--- src/commands/source/create_command.cpp
+++ src/commands/source/create_command.cpp
@@ -1,14 +1,18 @@
 #include "commands/create_command.hpp"
 
 #include "filesystem/file_record.hpp"
+
 std::string CreateCommand::execute() {
     if (!receiver_.is_valid()) {
         return ERROR + ": the file system has not been initialized";
     }
+
     auto& catalog = receiver_.get_catalog();
+
     if (catalog.get_files().contains(filename_)) {
         return FILE_ALREADY_EXISTS;
     }
+
     if (size_ > catalog.get_free_space()) {
         return NO_FREE_SPACE;
     }
@@ -28,7 +32,6 @@ std::string CreateCommand::execute() {
                         catalog.get_segments()[i].get_records()[j].set_size(size_);
                         catalog.get_files().insert(filename_);
                         record.set_type(filesystem::FileType::PERMANENT);
-                        record.set_filename(filename_);
                         catalog.get_free_space() -= size_;
                         --catalog.get_free_records();
                         for (size_t l = j + 1; l <= k; ++l) {
@@ -41,8 +44,8 @@ std::string CreateCommand::execute() {
             }
         }
     }
+    auto& last_segment = catalog.get_segments()[catalog.get_busy_segments_count()];
     if (catalog.get_busy_segments_count() < catalog.get_count() && size_ <= catalog.get_free_direct_space()) {
-        auto& last_segment = catalog.get_segments()[catalog.get_busy_segments_count()];
         if (last_segment.add_record(filename_, size_)) {
             ++catalog.get_busy_segments_count();
         }
--- src/commands/source/delete_command.cpp
+++ src/commands/source/delete_command.cpp
@@ -1,6 +1,7 @@
 #include "commands/delete_command.hpp"
 
 #include "filesystem/file_record.hpp"
+
 std::string DeleteCommand::execute() {
     if (!receiver_.is_valid()) {
         return ERROR + ": the file system has not been initialized";
@@ -15,31 +16,13 @@ std::string DeleteCommand::execute() {
         size_t size = record->get_size();
         catalog.get_free_space() += size;
         catalog.get_files().erase(filename_);
-        size_t last_segment_index = catalog.get_busy_segments_count();
-        filesystem::Segment* last_segment;
-        if (last_segment_index == catalog.get_count()) {
-            last_segment = &catalog.get_segments().back();
-            if (last_segment->get_records().back().get_type() == filesystem::FileType::FREE) {
-                last_segment->remove_record();
-                catalog.get_free_direct_space() += size;
-                --catalog.get_busy_segments_count();
-            }
-
-        } else {
-            if (catalog.get_segments()[last_segment_index].get_counter() == 0) {
-                last_segment = &catalog.get_segments()[last_segment_index - 1];
-                if (last_segment->get_records().back().get_type() == filesystem::FileType::FREE) {
-                    last_segment->remove_record();
-                    catalog.get_free_direct_space() += size;
-                    --catalog.get_busy_segments_count();
-                }
-            } else {
-                last_segment = &catalog.get_segments()[last_segment_index];
-                if (last_segment->get_records().back().get_type() == filesystem::FileType::FREE) {
-                    last_segment->remove_record();
-                    catalog.get_free_direct_space() += size;
-                }
-            }
+        auto* last_segment = &catalog.get_segments()[catalog.get_busy_segments_count()];
+        if (last_segment->get_counter() == 0) {
+            last_segment = &catalog.get_segments()[catalog.get_busy_segments_count() - 1];
+        }
+        if (last_segment->get_records().back().get_type() == filesystem::FileType::FREE) {
+            last_segment->remove_record();
+            catalog.get_free_direct_space() += size;
         }
         return OK + std::format(": the {} has been removed", filename_);
     }
