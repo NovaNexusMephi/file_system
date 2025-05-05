907787a535f4347d232b514073e59ec9e7f9c69f
10Narratives <vanyatertyshnikov@yandex.ru>
2025-05-06 00:11:13 +0300

    Update filesystem: add docs for system info

--- src/filesystem/include/filesystem/system_information.hpp
+++ src/filesystem/include/filesystem/system_information.hpp
@@ -4,32 +4,51 @@
 
 namespace filesystem {
 
+/**
+ * @brief Stores metadata and configuration information for the file system.
+ */
 class SystemInformation final {
    public:
+    /**
+     * @brief Default constructor. Initializes empty volume and owner names.
+     */
     SystemInformation() = default;
 
+    /**
+     * @brief Constructs a SystemInformation object with specified volume and owner.
+     * @param vol Name of the storage volume (moved into object).
+     * @param owner Name of the volume owner (moved into object).
+     */
     SystemInformation(std::string vol, std::string owner)
         : volume_name_(std::move(vol)), owner_name_(std::move(owner)) {}
 
-    inline void set_volume_name(std::string vol) noexcept { 
-        volume_name_ = std::move(vol); 
-    }
+    /**
+     * @brief Sets the volume name using move semantics.
+     * @param vol New volume name to assign.
+     */
+    inline void set_volume_name(std::string vol) noexcept { volume_name_ = std::move(vol); }
 
-    inline void set_owner_name(std::string owner) noexcept { 
-        owner_name_ = std::move(owner); 
-    }
+    /**
+     * @brief Sets the owner name using move semantics.
+     * @param owner New owner name to assign.
+     */
+    inline void set_owner_name(std::string owner) noexcept { owner_name_ = std::move(owner); }
 
-    [[nodiscard]] inline std::string get_volume_name() const noexcept { 
-        return volume_name_; 
-    }
+    /**
+     * @brief Retrieves the current volume name.
+     * @return Reference to the volume name string.
+     */
+    [[nodiscard]] inline std::string get_volume_name() const noexcept { return volume_name_; }
 
-    [[nodiscard]] inline std::string get_owner_name() const noexcept { 
-        return owner_name_; 
-    }
+    /**
+     * @brief Retrieves the current owner name.
+     * @return Reference to the owner name string.
+     */
+    [[nodiscard]] inline std::string get_owner_name() const noexcept { return owner_name_; }
 
    private:
-    std::string volume_name_;
-    std::string owner_name_;
+    std::string volume_name_;  ///< Name of the storage volume
+    std::string owner_name_;   ///< Name of the volume owner
 };
 
 }  // namespace filesystem
